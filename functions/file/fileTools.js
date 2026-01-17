8bc04292a4846efbe59a281edeba8a4773b002e0
kkjusdoit <kkjusdoit@users.noreply.github.com>
2026-01-18 00:50:51 +0800

    Revert "change"
    This reverts commit 6edd56f16ebb3ba0ed046d932d11b27fb69dcf95.

--- functions/file/fileTools.js
+++ functions/file/fileTools.js
@@ -189,9 +189,10 @@ export async function return404(url) {
 export async function returnBlockImg(url) {
     const blockImg = await fetch(url.origin + "/static/BlockImg.png");
     if (!blockImg.ok) {
-        return new Response("Access Denied: Blocked Image", {
-            status: 403,
+        return new Response(null, {
+            status: 302,
             headers: {
+                "Location": url.origin + "/blockimg",
                 "Cache-Control": "public, max-age=86400"
             }
         })
@@ -210,9 +211,10 @@ export async function returnBlockImg(url) {
 export async function returnWhiteListImg(url) {
     const WhiteListImg = await fetch(url.origin + "/static/WhiteListOn.png");
     if (!WhiteListImg.ok) {
-        return new Response("Access Denied: WhiteList Mode Enabled", {
-            status: 403,
+        return new Response(null, {
+            status: 302,
             headers: {
+                "Location": url.origin + "/whiteliston",
                 "Cache-Control": "public, max-age=86400"
             }
         })
